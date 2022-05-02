36c02703836e4b9b59258eb44c8d3a9673d75ed3
PauloLucena007 <paulo.henriquegvl@gmail.com>
2022-05-02 20:55:38 -0300

    fist commit

---
+++ fibo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Diagnostics;
+
+
+DateTime dataInicial = DateTime.Now;
+
+double fibo(double x)
+{
+    if (x > 1)
+    {
+        return fibo(x - 1) + fibo(x - 2);
+    }
+
+    return x;
+}
+
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine(fibo(i));
+};
+
+DateTime dataFinal = DateTime.Now;
+
+TimeSpan resultado = dataFinal - dataInicial;
+Console.WriteLine($"Resultado {resultado.TotalMilliseconds.ToString(CultureInfo.InvariantCulture)} Milliseconds");
+
+
+
+
